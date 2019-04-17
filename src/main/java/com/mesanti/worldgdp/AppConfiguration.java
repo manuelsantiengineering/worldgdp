b3f811a51cff398fbf1eac7562dd5fc287705043
Santiago Laboy <msanti04@harris.com>
2019-04-17 11:50:49 -0400

    Changed maving compiler plugin and set the maven jdk build version to 1.8

---
+++ src/main/java/com/mesanti/worldgdp/AppConfiguration.java
@@ -0,0 +1,19 @@
+package com.mesanti.worldgdp;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.mesanti.worldgdp")
+public class AppConfiguration  implements WebMvcConfigurer{
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+	
+}
