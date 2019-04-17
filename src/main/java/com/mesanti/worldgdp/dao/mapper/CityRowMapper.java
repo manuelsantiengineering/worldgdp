afbdd78758eb065746744a83690d9831dae407bb
Santiago Laboy <msanti04@harris.com>
2019-04-17 11:31:22 -0400

    Created the external connection with the WB api

---
+++ src/main/java/com/mesanti/worldgdp/dao/mapper/CityRowMapper.java
@@ -0,0 +1,23 @@
+package com.mesanti.worldgdp.dao.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.mesanti.worldgdp.model.City;
+
+public class CityRowMapper implements RowMapper<City>{
+
+	public City mapRow(ResultSet rs, int rowNum) throws SQLException {
+		City city = new City();
+		city.setCountryCode(rs.getString("country_code"));
+		city.setDistrict(rs.getString("district"));
+		city.setId(rs.getLong("id"));
+		city.setName(rs.getString("name"));
+		city.setPopulation(rs.getLong("population"));
+		return city;
+
+	}
+
+}
